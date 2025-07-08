5aaa97f6ad43eef1e2d95b69cb7b27a35e2ce4aa
paulina-pacheco-o <paulimichi05@gmail.com>
2025-07-08 18:42:57 +0200

    third exercise

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,17 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function nameWithA() {
+  for (let i = 0; i < names.length; i++) {
+    const letter = names[i].charAt(0);
+    if (letter === "A") {
 
+    }
+  }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+nameWithA()
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
